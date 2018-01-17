18d1c423eba3cc21ec7cc729c21563e11ab9397f
Ralph Castain <rhc@open-mpi.org>
2018-01-17 06:43:47 -0800

    Update merge
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- opal/runtime/opal_progress.c
+++ opal/runtime/opal_progress.c
@@ -15,7 +15,7 @@
  * Copyright (c) 2015-2016 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  *
- * Copyright (c) 2017      Intel, Inc. All rights reserved.
+ * Copyright (c) 2017-2018 Intel, Inc. All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
