5761641d50835872cd391376a801fe9aec0ed235
Gilles Gouaillardet <gilles@rist.or.jp>
2018-06-19 09:13:35 +0900

    travis: more debug
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- contrib/travis/install-pmix.sh
+++ contrib/travis/install-pmix.sh
@@ -3,5 +3,7 @@ set -ex
 git clone --depth=1 https://github.com/pmix/pmix pmix-master
 cd pmix-master
 which gcc
+type gcc
+alias gcc
 gcc --version
 ./autogen.pl && ./configure --prefix=/usr && cat src/include/pmix_config.h && make V=1 && sudo make install
