2d6ad183d91b44fb8a446fe3938a23c78e1eea02
Gilles Gouaillardet <gilles@rist.or.jp>
2018-06-19 09:16:48 +0900

    more debug
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- contrib/travis/install-pmix.sh
+++ contrib/travis/install-pmix.sh
@@ -4,6 +4,7 @@ git clone --depth=1 https://github.com/pmix/pmix pmix-master
 cd pmix-master
 which gcc
 type gcc
-alias gcc
+ls -l /usr/bin/gcc
+ls -l /usr/bin/gcc-4.8 || true
 gcc --version
 ./autogen.pl && ./configure --prefix=/usr && cat src/include/pmix_config.h && make V=1 && sudo make install
