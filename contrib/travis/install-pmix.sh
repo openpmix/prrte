84b7a7bf22018f49a5d9928b4ff0ac87d360b521
Gilles Gouaillardet <gilles@rist.or.jp>
2018-06-19 09:06:02 +0900

    travis: only git clone --depth=1 pmix
    and save some time/space/bandwidth
    
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- contrib/travis/install-pmix.sh
+++ contrib/travis/install-pmix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-git clone https://github.com/pmix/pmix pmix-master
+git clone --depth=1 https://github.com/pmix/pmix pmix-master
 cd pmix-master
 ./autogen.pl && ./configure --prefix=/usr --disable-picky && make && sudo make install
