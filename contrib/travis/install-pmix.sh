6cae772d855d822c6ffd185603885865adfd3419
Gilles Gouaillardet <gilles@rist.or.jp>
2018-10-29 10:43:10 +0900

    test
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- contrib/travis/install-pmix.sh
+++ contrib/travis/install-pmix.sh
@@ -2,4 +2,4 @@
 set -ex
 git clone --depth=1 https://github.com/pmix/pmix pmix-master
 cd pmix-master
-./autogen.pl && ./configure --prefix=/usr --disable-picky && make && sudo make install
+./autogen.pl && ./configure --prefix=/usr --disable-picky $* && make && sudo make install
