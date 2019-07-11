837c95987eafa8c3ec2599a504bbdc355cce0851
Ralph Castain <rhc@pmix.org>
2019-07-11 10:56:05 -0700

    Cleanup and fix the lib versioning
    Remove unnecessary abstractions and update VERSION
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- opal/include/opal_config_bottom.h
+++ opal/include/opal_config_bottom.h
@@ -15,7 +15,7 @@
  *                         All rights reserved.
  * Copyright (c) 2015-2017 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
- * Copyright (c) 2015-2018 Intel, Inc. All rights reserved.
+ * Copyright (c) 2015-2019 Intel, Inc.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
