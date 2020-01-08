332fde74da88280f032b41d1395127ca420286b4
Ralph Castain <rhc@pmix.org>
2020-01-08 07:42:16 -0800

    Try to debug Travis test hang
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- contrib/travis/test_client.sh
+++ contrib/travis/test_client.sh
@@ -3,12 +3,13 @@
 cd examples
 export PATH=$HOME/bogus/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/bogus/lib:$HOME/bogus/lib/pmix:/usr/lib:/usr/lib/pmix:$LD_LIBRARY_PATH
+export PMIX_MCA_pmix_client_event_verbose=5 PMIX_MCA_pmix_server_event_verbose=5
 
 make client
 
 set -x
 
-prte --daemonize --host localhost:5
+prte --daemonize --host localhost:5 --mca state_base_verbose 10
 
 prun -n 2 ./client
 ret=$?
