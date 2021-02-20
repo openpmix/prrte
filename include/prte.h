34e79c33b6d32475afaee68aa73d7ee8db17f38f
Ralph Castain <rhc@pmix.org>
2021-02-20 11:01:53 -0800

    Create and install a prte_version.h header
    Allow external packages to detect what version of PRRTE
    this is - if the header is not found, then they can safely
    assume it is version 1.0.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

---
+++ include/prte.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016      Mellanox Technologies, Inc.
+ *                         All rights reserved.
+ * Copyright (c) 2018      IBM Corporation.  All rights reserved.
+ * Copyright (c) 2018      Intel, Inc. All rights reserved.
+ * Copyright (c) 2019      Research Organization for Information Science
+ *                         and Technology (RIST).  All rights reserved.
+ * Copyright (c) 2021      Nanook Consulting  All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+
+#ifndef PRTE_H
+#define PRTE_H
+
+#include "prte_version.h"
+
+#endif
