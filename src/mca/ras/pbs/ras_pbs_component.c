440e188dca9f8b3f750bd73b7f5ea38e05778d00
Ralph Castain <rhc@pmix.org>
2022-01-27 13:48:18 -0800

    Always build the PBS scheduler support
    Unless directed not to do so. Note this is separate
    from the Torque launcher, which still only builds if
    the TM libraries are found.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/mca/ras/pbs/ras_pbs_component.c
+++ src/mca/ras/pbs/ras_pbs_component.c
@@ -16,7 +16,7 @@
  *                         reserved.
  * Copyright (c) 2019      Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
- * Copyright (c) 2021      Nanook Consulting.  All rights reserved.
+ * Copyright (c) 2021-2022 Nanook Consulting.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -31,7 +31,7 @@
 #include "src/mca/base/prte_mca_base_var.h"
 #include "src/util/basename.h"
 
-#include "ras_tm.h"
+#include "ras_pbs.h"
 #include "src/mca/ras/base/ras_private.h"
 
 /*
@@ -42,11 +42,11 @@ static int param_priority;
 /*
  * Local functions
  */
-static int ras_tm_register(void);
-static int ras_tm_open(void);
-static int prte_ras_tm_component_query(prte_mca_base_module_t **module, int *priority);
+static int ras_pbs_register(void);
+static int ras_pbs_open(void);
+static int prte_ras_pbs_component_query(prte_mca_base_module_t **module, int *priority);
 
-prte_ras_tm_component_t prte_ras_tm_component = {
+prte_ras_pbs_component_t prte_ras_pbs_component = {
     {
         /* First, the prte_mca_base_component_t struct containing meta
            information about the component itself */
@@ -55,14 +55,14 @@ prte_ras_tm_component_t prte_ras_tm_component = {
             PRTE_RAS_BASE_VERSION_2_0_0,
 
             /* Component name and version */
-            .mca_component_name = "tm",
+            .mca_component_name = "pbs",
             PRTE_MCA_BASE_MAKE_VERSION(component, PRTE_MAJOR_VERSION, PRTE_MINOR_VERSION,
                                         PRTE_RELEASE_VERSION),
 
             /* Component open and close functions */
-            .mca_open_component = ras_tm_open,
-            .mca_query_component = prte_ras_tm_component_query,
-            .mca_register_component_params = ras_tm_register,
+            .mca_open_component = ras_pbs_open,
+            .mca_query_component = prte_ras_pbs_component_query,
+            .mca_register_component_params = ras_pbs_register,
         },
         .base_data = {
             /* The component is checkpoint ready */
@@ -71,35 +71,15 @@ prte_ras_tm_component_t prte_ras_tm_component = {
     }
 };
 
-static int ras_tm_register(void)
+static int ras_pbs_register(void)
 {
-    prte_mca_base_component_t *c = &prte_ras_tm_component.super.base_version;
-    char *pbs_nodefile_env = NULL;
+    prte_mca_base_component_t *c = &prte_ras_pbs_component.super.base_version;
 
     param_priority = 100;
-    (void) prte_mca_base_component_var_register(c, "priority", "Priority of the tm ras component",
+    (void) prte_mca_base_component_var_register(c, "priority", "Priority of the pbs ras component",
                                                 PRTE_MCA_BASE_VAR_TYPE_INT, NULL, 0,
                                                 PRTE_MCA_BASE_VAR_FLAG_NONE, PRTE_INFO_LVL_9,
                                                 PRTE_MCA_BASE_VAR_SCOPE_READONLY, &param_priority);
-
-    prte_ras_tm_component.nodefile_dir = NULL;
-
-    /* try to detect the default directory */
-    pbs_nodefile_env = getenv("PBS_NODEFILE");
-    if (NULL != pbs_nodefile_env) {
-        prte_ras_tm_component.nodefile_dir = prte_dirname(pbs_nodefile_env);
-    }
-
-    if (NULL == prte_ras_tm_component.nodefile_dir) {
-        prte_ras_tm_component.nodefile_dir = strdup("/var/torque/aux");
-    }
-
-    (void) prte_mca_base_component_var_register(c, "nodefile_dir",
-                                                "The directory where the PBS nodefile can be found",
-                                                PRTE_MCA_BASE_VAR_TYPE_STRING, NULL, 0,
-                                                PRTE_MCA_BASE_VAR_FLAG_NONE, PRTE_INFO_LVL_9,
-                                                PRTE_MCA_BASE_VAR_SCOPE_READONLY,
-                                                &prte_ras_tm_component.nodefile_dir);
 
     /* for big SMP machines (e.g., those from SGI), listing the nodes
      * once/slot in the nodefile is extreme. In those cases, they may
@@ -107,28 +87,28 @@ static int ras_tm_register(void)
      * tells us how many cpus/node were allocated. Allow the user to
      * inform us that we are in such an environment
      */
-    prte_ras_tm_component.smp_mode = false;
+    prte_ras_pbs_component.smp_mode = false;
     (void) prte_mca_base_component_var_register(
         c, "smp",
-        "The Torque system is configured in SMP mode "
+        "The PBS system is configured in SMP mode "
         "with the number of cpus/node given in the environment",
         PRTE_MCA_BASE_VAR_TYPE_BOOL, NULL, 0, PRTE_MCA_BASE_VAR_FLAG_NONE, PRTE_INFO_LVL_9,
-        PRTE_MCA_BASE_VAR_SCOPE_READONLY, &prte_ras_tm_component.smp_mode);
+        PRTE_MCA_BASE_VAR_SCOPE_READONLY, &prte_ras_pbs_component.smp_mode);
 
     return PRTE_SUCCESS;
 }
 
-static int ras_tm_open(void)
+static int ras_pbs_open(void)
 {
     return PRTE_SUCCESS;
 }
 
-static int prte_ras_tm_component_query(prte_mca_base_module_t **module, int *priority)
+static int prte_ras_pbs_component_query(prte_mca_base_module_t **module, int *priority)
 {
-    /* Are we running under a TM job? */
+    /* Are we running under a PBS job? */
     if (NULL != getenv("PBS_ENVIRONMENT") && NULL != getenv("PBS_JOBID")) {
         *priority = param_priority;
-        *module = (prte_mca_base_module_t *) &prte_ras_tm_module;
+        *module = (prte_mca_base_module_t *) &prte_ras_pbs_module;
         return PRTE_SUCCESS;
     }
 
