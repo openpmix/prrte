ad2bc4e2b888c7ed8203710a2335fae3954d7f1b
Ralph Castain <rhc@pmix.org>
2022-04-07 15:39:40 -0700

    Correctly add qualifiers to options
    Delimiter must be a colon, not a comma
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/mca/schizo/base/schizo_base_stubs.c
+++ src/mca/schizo/base/schizo_base_stubs.c
@@ -181,7 +181,7 @@ int prte_schizo_base_add_qualifier(pmix_cli_result_t *results,
             // does it already contain a qualifier?
             if (NULL != strchr(opt->values[0], ':')) {
                 // can just add this one to the end
-                pmix_asprintf(&tmp, "%s,%s", opt->values[0], qualifier);
+                pmix_asprintf(&tmp, "%s:%s", opt->values[0], qualifier);
                 free(opt->values[0]);
                 opt->values[0] = tmp;
             } else {
