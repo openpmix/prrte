126be9efc0f36f8e8e6a39ed83857632764fd916
Ralph Castain <rhc@pmix.org>
2022-08-08 07:17:48 -0700

    Remove stale files
    PRRTE no longer provides man pages - the help system replaces
    them. Remove stale contrib files that don't pertain to PRRTE.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 8112ac885603b1c1878c02374244c4607cd24772)

--- contrib/travis/install-pmix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-git clone --depth=1 https://github.com/pmix/pmix pmix-master
-cd pmix-master
-./autogen.pl && ./configure --prefix=/usr --disable-picky $* && make && sudo make install
