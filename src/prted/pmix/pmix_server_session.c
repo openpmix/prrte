f6ded45e4c04dc8f97a2354224c14eccae7d89aa
Ralph Castain <rhc@pmix.org>
2022-12-17 07:25:58 -0800

    Check for and track scheduler attachment
    Record that a scheduler has attached to us so
    we know when scheduler-related requests can be
    supported.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit a5c3571a7bfa38c7e448d5b790d5809f5929f218)

--- src/prted/pmix/pmix_server_session.c
+++ src/prted/pmix/pmix_server_session.c
@@ -17,12 +17,27 @@ pmix_status_t pmix_server_alloc_fn(const pmix_proc_t *client,
                                    const pmix_info_t data[], size_t ndata,
                                    pmix_info_cbfunc_t cbfunc, void *cbdata)
 {
+    pmix_status_t rc;
+
     /* if we are not the DVM controller, then send it there */
 
     /* we are the DVM controller, so pass it down so PMIx can
      * send it to the scheduler */
 
-    return PMIX_ERR_NOT_SUPPORTED;
+    if (!prte_pmix_server_globals.scheduler_connected) {
+        /* the scheduler has not attached to us - there is
+         * nothing we can do */
+        return PMIX_ERR_NOT_SUPPORTED;
+    }
+    /* if we have not yet set the scheduler as our server, do so */
+    if (!prte_pmix_server_globals.scheduler_set_as_server) {
+        rc = PMIx_tool_set_server(&prte_pmix_server_globals.scheduler, NULL, 0);
+        if (PMIX_SUCCESS != rc) {
+            return rc;
+        }
+        prte_pmix_server_globals.scheduler_set_as_server = true;
+    }
+
 }
 
 #if PMIX_NUMERIC_VERSION >= 0x00050000
@@ -32,12 +47,27 @@ pmix_status_t pmix_server_session_ctrl_fn(const pmix_proc_t *requestor,
                                           const pmix_info_t directives[], size_t ndirs,
                                           pmix_info_cbfunc_t cbfunc, void *cbdata)
 {
+    pmix_status_t rc;
+
     /* if we are not the DVM controller, then send it there */
 
     /* we are the DVM controller, so pass it down so PMIx can
      * send it to the scheduler */
 
-    return PMIX_ERR_NOT_SUPPORTED;
+    if (!prte_pmix_server_globals.scheduler_connected) {
+        /* the scheduler has not attached to us - there is
+         * nothing we can do */
+        return PMIX_ERR_NOT_SUPPORTED;
+    }
+    /* if we have not yet set the scheduler as our server, do so */
+    if (!prte_pmix_server_globals.scheduler_set_as_server) {
+        rc = PMIx_tool_set_server(&prte_pmix_server_globals.scheduler, NULL, 0);
+        if (PMIX_SUCCESS != rc) {
+            return rc;
+        }
+        prte_pmix_server_globals.scheduler_set_as_server = true;
+    }
+
 }
 
 #endif
