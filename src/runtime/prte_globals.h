cbc2c5815cdd51e3d7129ec9b97a5337a86427b2
Ralph Castain <rhc@pmix.org>
2024-11-22 10:03:25 -0700

    Enclose XML output in a high-level tag
    Trap output between a high-level tag
    (e.g., <prterun>...</prterun>). Note that
    show_help output will still not be XML formatted,
    and any output prior to setting up the IOF
    subsystem will also not be formatted.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 91c77cd5bd0a3218b11f18f7d4f9edfcb263cefe)

--- src/runtime/prte_globals.h
+++ src/runtime/prte_globals.h
@@ -510,6 +510,7 @@ PRTE_EXPORT extern pmix_pointer_array_t *prte_cache;
 PRTE_EXPORT extern bool prte_persistent;
 PRTE_EXPORT extern bool prte_allow_run_as_root;
 PRTE_EXPORT extern bool prte_fwd_environment;
+PRTE_EXPORT extern bool prte_xml_output;
 
 /* PRTE OOB port flags */
 PRTE_EXPORT extern bool prte_static_ports;
