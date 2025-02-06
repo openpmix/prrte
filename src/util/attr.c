1b219127d7f7a9a4a132ed30cc7a31e668e64d89
Ralph Castain <rhc@pmix.org>
2025-02-06 08:37:56 -0700

    Add ability to direct app GPU support
    There apparently are some circumstances when an application can
    benefit from disabling the internal GPU support in one or more
    of its libraries. Let's assume that a library might also provide
    a mechanism by which that support can be defaulted to enabled
    or disabled.
    
    Add CLI support for specifying that GPU support be enabled or
    disabled. We assume that:
    
    (a) this is something that a tool might want to enquire about
        to see what an app was told to do
    
    (b) a user might want/expect this to be a directive inherited
        by any spawned child jobs
    
    Also note that there was a lot of code duplication between
    prte and prun_common when it came to parsing the cmd line
    for job-level directives. Collect those in a common function
    as we see that some divergence had already occurred.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 0b74d3a917fe0a6b87b5679a5e665c326f4544c3)

--- src/util/attr.c
+++ src/util/attr.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2014-2017 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  * Copyright (c) 2018-2020 Cisco Systems, Inc.  All rights reserved
- * Copyright (c) 2021-2023 Nanook Consulting.  All rights reserved.
+ * Copyright (c) 2021-2025 Nanook Consulting  All rights reserved.
  * Copyright (c) 2021      The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
@@ -501,6 +501,8 @@ const char *prte_attr_key_to_str(prte_attribute_key_t key)
             return "EXTEND DVM";
         case PRTE_JOB_CHILD_SEP:
             return "CHILD SEP";
+        case PRTE_JOB_GPU_SUPPORT:
+            return "GPU SUPPORT";
 
         case PRTE_PROC_NOBARRIER:
             return "PROC-NOBARRIER";
