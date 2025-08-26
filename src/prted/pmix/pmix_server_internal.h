56b83ba8d3bd8ab4cf54c48128b28f1b8a543d53
Ralph Castain <rhc@pmix.org>
2025-08-26 12:41:18 -0600

    Properly implement the "abort" operation
    When a tool or proc calls abort, we need to propagate that
    request to the HNP for proper execution. If the call comes
    from a tool, then we need to ensure we don't terminate the
    DVM until after the tool disconnects.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 2278a7122a0b41216b90098eaacef6a00b6f8782)

--- src/prted/pmix/pmix_server_internal.h
+++ src/prted/pmix/pmix_server_internal.h
@@ -128,15 +128,10 @@ typedef struct {
     pmix_info_cbfunc_t infocbfunc;
     pmix_tool_connection_cbfunc_t toolcbfunc;
     pmix_spawn_cbfunc_t spcbfunc;
+    pmix_event_notification_cbfunc_fn_t evcbfunc;
     void *cbdata;
 } prte_pmix_server_op_caddy_t;
 PMIX_CLASS_DECLARATION(prte_pmix_server_op_caddy_t);
-
-typedef struct {
-    pmix_list_item_t super;
-    pmix_proc_t name;
-} prte_pmix_tool_t;
-PMIX_CLASS_DECLARATION(prte_pmix_tool_t);
 
 #define PRTE_IO_OP(t, nt, b, fn, cfn, cbd)                                         \
     do {                                                                           \
@@ -312,15 +307,50 @@ PRTE_EXPORT extern void pmix_server_notify(int status, pmix_proc_t *sender,
                                            pmix_data_buffer_t *buffer, prte_rml_tag_t tg,
                                            void *cbdata);
 
-PRTE_EXPORT extern void pmix_server_jobid_return(int status, pmix_proc_t *sender,
-                                           pmix_data_buffer_t *buffer, prte_rml_tag_t tg,
-                                           void *cbdata);
+PRTE_EXPORT extern void pmix_server_tconn_return(int status, pmix_proc_t *sender,
+                                                 pmix_data_buffer_t *buffer, prte_rml_tag_t tg,
+                                                 void *cbdata);
 
-PRTE_EXPORT extern int prte_pmix_server_register_tool(pmix_nspace_t nspace);
+PRTE_EXPORT extern int prte_pmix_server_register_tool(pmix_server_req_t *cd);
 
 PRTE_EXPORT extern int pmix_server_cache_job_info(prte_job_t *jdata, pmix_info_t *info);
 
+PRTE_EXPORT extern int prte_pmix_xfer_job_info(prte_job_t *jdata,
+                                               pmix_info_t *iptr,
+                                               size_t ninfo);
 #if PMIX_NUMERIC_VERSION >= 0x00050000
+
+PRTE_EXPORT pmix_status_t pmix_server_session_ctrl_fn(const pmix_proc_t *requestor,
+                                                      uint32_t sessionID,
+                                                      const pmix_info_t directives[], size_t ndirs,
+                                                      pmix_info_cbfunc_t cbfunc, void *cbdata);
+#endif
+
+PRTE_EXPORT extern int prte_pmix_xfer_app(prte_job_t *jdata, pmix_app_t *app);
+
+PRTE_EXPORT extern void pmix_server_alloc_request_resp(int status, pmix_proc_t *sender,
+                                                pmix_data_buffer_t *buffer, prte_rml_tag_t tg,
+                                                void *cbdata);
+
+PRTE_EXPORT extern pmix_status_t prte_pmix_set_scheduler(void);
+
+PRTE_EXPORT extern pmix_status_t prte_server_send_request(uint8_t cmd, pmix_server_req_t *req);
+
+PRTE_EXPORT extern void prte_server_lost_connection(size_t evhdlr_registration_id,
+                                                    pmix_status_t status,
+                                                    const pmix_proc_t *source,
+                                                    pmix_info_t info[], size_t ninfo,
+                                                    pmix_info_t *results, size_t nresults,
+                                                    pmix_event_notification_cbfunc_fn_t cbfunc,
+                                                    void *cbdata);
+
+
+#define PRTE_PMIX_ALLOC_REQ      0
+#define PRTE_PMIX_SESSION_CTRL   1
+
+
+#ifdef PMIX_SESSION_INSTANTIATE
+
 PRTE_EXPORT extern pmix_status_t
 pmix_server_session_ctrl_fn(const pmix_proc_t *requestor,
                             uint32_t sessionID,
@@ -359,7 +389,6 @@ typedef struct {
     char *report_uri;
     char *singleton;
     pmix_device_type_t generate_dist;
-    pmix_list_t tools;
     pmix_list_t psets;
     pmix_list_t groups;
 } pmix_server_globals_t;
