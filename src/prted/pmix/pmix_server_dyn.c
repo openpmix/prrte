aa8ab331120bf88565ecc545d1210a224d16a1ec
Ralph Castain <rhc@pmix.org>
2025-09-26 12:57:17 -0600

    Fix precedence ordering on envar operations
    Work from left-to-right across the cmd line, applying env-related
    options as we go. When one operation affects the result of another,
    this preserves a user's common expectation.
    
    Add a "--set-env" option if the corresponding PMIx CLI is defined.
    Seemed a little weird that we had "prepend-env", "append-env", etc.,
    but no "set-env". It's the equivalent of "-x foo=val".
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 805e1302bd2e0bad2f9aff81e421abeacd3a73a5)

--- src/prted/pmix/pmix_server_dyn.c
+++ src/prted/pmix/pmix_server_dyn.c
@@ -787,29 +787,30 @@ static void interim(int sd, short args, void *cbdata)
             envar.envar = info->value.data.envar.envar;
             envar.value = info->value.data.envar.value;
             envar.separator = info->value.data.envar.separator;
-            prte_prepend_attribute(&jdata->attributes, PRTE_JOB_SET_ENVAR,
-                                   PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
+            prte_set_attribute(&jdata->attributes, PRTE_JOB_SET_ENVAR,
+                               PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
         } else if (PMIX_CHECK_KEY(info, PMIX_ADD_ENVAR)) {
             envar.envar = info->value.data.envar.envar;
             envar.value = info->value.data.envar.value;
             envar.separator = info->value.data.envar.separator;
-            prte_prepend_attribute(&jdata->attributes, PRTE_JOB_ADD_ENVAR,
-                                   PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
+            prte_set_attribute(&jdata->attributes, PRTE_JOB_ADD_ENVAR,
+                               PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
         } else if (PMIX_CHECK_KEY(info, PMIX_UNSET_ENVAR)) {
-            prte_prepend_attribute(&jdata->attributes, PRTE_JOB_UNSET_ENVAR,
-                                   PRTE_ATTR_GLOBAL, info->value.data.string, PMIX_STRING);
+            prte_set_attribute(&jdata->attributes, PRTE_JOB_UNSET_ENVAR,
+                               PRTE_ATTR_GLOBAL, info->value.data.string, PMIX_STRING);
         } else if (PMIX_CHECK_KEY(info, PMIX_PREPEND_ENVAR)) {
             envar.envar = info->value.data.envar.envar;
             envar.value = info->value.data.envar.value;
             envar.separator = info->value.data.envar.separator;
-            prte_prepend_attribute(&jdata->attributes, PRTE_JOB_PREPEND_ENVAR,
-                                   PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
+            prte_set_attribute(&jdata->attributes, PRTE_JOB_PREPEND_ENVAR,
+                               PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
         } else if (PMIX_CHECK_KEY(info, PMIX_APPEND_ENVAR)) {
             envar.envar = info->value.data.envar.envar;
             envar.value = info->value.data.envar.value;
             envar.separator = info->value.data.envar.separator;
-            prte_prepend_attribute(&jdata->attributes, PRTE_JOB_APPEND_ENVAR,
-                                   PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
+            prte_set_attribute(&jdata->attributes, PRTE_JOB_APPEND_ENVAR,
+                               PRTE_ATTR_GLOBAL, &envar, PMIX_ENVAR);
+
         } else if (PMIX_CHECK_KEY(info, PMIX_SPAWN_TOOL)) {
             PRTE_FLAG_SET(jdata, PRTE_JOB_FLAG_TOOL);
 
