84140d24be9dd100b1aa604f0482708474bdabf2
Ralph Castain <rhc@pmix.org>
2025-10-26 13:58:52 -0600

    Do not assign DVM's bookmark to the application job
    Allow the target node list to follow the ordering inside a provided hostfile
    and dash-host specification by not assigning a bookmark based on the DVM job.
    
    Add support for missing default-hostfile cmd line option We have the support
    for the user to specify it via MCA param, but somehow we lost the integration
    to pick it up off of the prte and prterun cmd lines.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 16d84129e7d9ff277bb5f21eeb89236bbf31371b)

--- src/mca/plm/base/plm_base_receive.c
+++ src/mca/plm/base/plm_base_receive.c
@@ -375,7 +375,8 @@ void prte_plm_base_recv(int status, pmix_proc_t *sender,
             goto ANSWER_LAUNCH;
         }
 
-        if (NULL != parent && !PRTE_FLAG_TEST(parent, PRTE_JOB_FLAG_TOOL)) {
+        if (NULL != parent && !PRTE_FLAG_TEST(parent, PRTE_JOB_FLAG_TOOL) &&
+            !PMIX_CHECK_NSPACE(parent->nspace, PRTE_PROC_MY_NAME->nspace)) {
             if (NULL == parent->bookmark) {
                 /* find the sender's node in the job map */
                 proc = (prte_proc_t *) pmix_pointer_array_get_item(parent->procs, sender->rank);
