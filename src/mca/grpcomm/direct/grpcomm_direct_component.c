0bf51e9e2086e5fd7b6f1d2b6f1372f11ae55981
Ralph Castain <rhc@pmix.org>
2026-02-01 07:42:11 -0700

    Initialize variable
    Initialize the "bootstrap" flag to false in the grpcomm
    struct for tracking group operations.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/mca/grpcomm/direct/grpcomm_direct_component.c
+++ src/mca/grpcomm/direct/grpcomm_direct_component.c
@@ -6,7 +6,7 @@
  * Copyright (c) 2014-2019 Intel, Inc.  All rights reserved.
  * Copyright (c) 2019      Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
- * Copyright (c) 2021-2025 Nanook Consulting  All rights reserved.
+ * Copyright (c) 2021-2026 Nanook Consulting  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -135,6 +135,7 @@ static void gccon(prte_grpcomm_group_t *p)
     p->status = PMIX_SUCCESS;
     p->dmns = NULL;
     p->ndmns = 0;
+    p->bootstrap = false;
     p->nexpected = 0;
     p->nreported = 0;
     p->nleaders = 0;
