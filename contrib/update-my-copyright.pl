427729f91617be56654023fb3ae43e7de45aee44
Jeff Squyres <jeff@squyres.com>
2026-09-24 15:02:48 -0400

    update-my-copyright.pl: restore @protected and remove --manual-list
    64f292175c ("update-my-copyright.py: properly support git
    workspaces") removed the Mercurial, Subversion, and manual-list
    support, and with it the declarations of $my_manual_list and
    @protected.  But GetOptions() still binds --manual-list to
    $my_manual_list, and the main loop still iterates over @protected, so
    under "use strict" the script no longer compiles:
    
        Global symbol "$my_manual_list" requires explicit package name ...
        Global symbol "@protected" requires explicit package name ...
    
    (followed by a long list of "String found where operator expected"
    errors about quiet_print, which are fallout from the above rather
    than a separate problem).  Anyone who runs the script from a git
    pre-commit hook cannot commit to PRRTE at all.
    
    Restore the @protected list, which keeps the script from editing
    itself; the entries it used to have besides the script were paths to
    embedded packages in Open MPI's tree, which do not exist here.
    
    Remove the --manual-list option and its --help text: the code that
    read the list was removed in 64f292175c, so the option could not work
    anyway.  Also update the comment that described finding changed files
    with "svn st" / "hg st" -- it is "git status" now -- and fix the
    --help text, which documented --formal-name as --formal-same.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- contrib/update-my-copyright.pl
+++ contrib/update-my-copyright.pl
@@ -4,6 +4,7 @@
 # Copyright (c) 2016-2017 Intel, Inc. All rights reserved.
 # Copyright (c) 2017      IBM Corporation. All rights reserved.
 # Copyright (c) 2026      Nanook Consulting  All rights reserved.
+# Copyright (c) 2026      Jeffrey M. Squyres.  All rights reserved.
 # $COPYRIGHT$
 #
 
@@ -21,8 +22,7 @@
 # More details:
 #
 # This is a simple script to traverse the tree looking for added and
-# changed files (via "svn st ." or "hg st .", depending on what meta
-# directory is found in this tree).  Note that the search starts in
+# changed files (via "git status").  Note that the search starts in
 # the current directory -- not the top-level directory.
 #
 # All added and changed files are examined.  If the special
@@ -69,6 +69,12 @@ my $HELP = 0;
 my $my_search_name = "Nanook";
 my $my_formal_name = "Nanook Consulting.  All rights reserved.";
 
+# Protected files: regular expressions matched against the names of
+# files that this script must never edit
+my @protected = qw(
+    contrib\\/update-my-copyright.pl
+);
+
 # Override the defaults if some values are set in the environment
 $my_search_name = $ENV{PRTE_COPYRIGHT_SEARCH_NAME}
     if (defined($ENV{PRTE_COPYRIGHT_SEARCH_NAME}));
@@ -81,7 +87,6 @@ GetOptions(
     "check-only" => \$CHECK_ONLY,
     "search-name=s" => \$my_search_name,
     "formal-name=s" => \$my_formal_name,
-    "manual-list=s" => \$my_manual_list,
 ) or die "unable to parse options, stopped";
 
 if ($HELP) {
@@ -92,8 +97,7 @@ $0 [options]
 --quiet | -q         Only output critical messages to stdout
 --check-only         exit(111) if there are files with copyrights to edit
 --search-name=NAME   Set search name to NAME
---formal-same=NAME   Set formal name to NAME
---manual-list=FNAME  Use specified file as list of files to mod copyright
+--formal-name=NAME   Set formal name to NAME
 EOT
     exit(0);
 }
